1d9dff62e72d783beeaee729342291ec71241958
4sela <adawa21@student.sdu.dk>
2024-07-09 03:03:15 +0200

    + Fine tuning

--- Assets/_Scripts/MedKitViewer.cs
+++ Assets/_Scripts/MedKitViewer.cs
@@ -3,86 +3,105 @@ using UnityEngine.Serialization;
 
 sealed class MedKitViewer : MonoBehaviour
 {
-    [SerializeField] private PlayerStats _playerStats;
+    [SerializeField] private PlayerHealth _playerHealth;
     [SerializeField] private MedKitBar _medKitBar;
     [SerializeField] private uint _numberOfMedKits;
-    private bool _full;
-    private bool _empty;
-    
+    private bool _fullMedKitInventory;
+    private bool _emptyMedKitInventory;
+
     void Start()
     {
         _medKitBar.SetMedKitCount(_numberOfMedKits);
     }
 
-    
     void Update()
     {
         FullOrEmptyChecker();
-        
+
+        /*
+         *      The if-statements with an Input.GetKey() method are placeholder logic to demonstrate the functions
+         *      that picks up and uses Med Kits to heal the player.
+         *
+         *      MUST BE REMOVED OR REPLACED AT A LATER TIME!!
+         */
+
+        #region REMOVE THESE IF-STATEMENTS ONCE A PERMANENT SOLUTION IS PUT IN PLACE.
+
         if (Input.GetKeyDown(KeyCode.O))
         {
-            PickUpMedKit(); 
+            PickUpMedKit();
         }
 
         else if (Input.GetKeyDown(KeyCode.P))
         {
             UseMedKit();
         }
+
+        #endregion
     }
-    
+
     private void UseMedKit()
     {
-        if (_playerStats._IsAtMaxHealth)
+        if (_playerHealth._IsDead)
+        {
+            return;
+        }
+
+        if (_playerHealth._IsAtMaxHealth)
         {
             print("Your health is already at maximum.");
             return;
         }
 
-        if (_empty)
+        if (_emptyMedKitInventory)
         {
             print("You have no Med Kits to use.");
             return;
         }
-        
+
         _numberOfMedKits -= 1;
         print("You have successfully gained full health.");
-        _playerStats.GainHealth();
+        _playerHealth.GainHealth();
         UpdateMedKitBar();
     }
 
-    
     private void PickUpMedKit()
     {
-        if (_full)
+        if (_playerHealth._IsDead)
+        {
+            return;
+        }
+
+        if (_fullMedKitInventory)
         {
             print("Your Med Kit inventory is already full.");
             return;
         }
-        
+
         _numberOfMedKits += 1;
         print("A Med Kit has been picked up.");
         UpdateMedKitBar();
     }
-    
+
     private void FullOrEmptyChecker()
     {
         switch (_numberOfMedKits)
         {
             case 0:
-                _full = false;
-                _empty = true;
+                _fullMedKitInventory = false;
+                _emptyMedKitInventory = true;
                 break;
             case 3:
-                _full = true;
-                _empty = false;
+                _fullMedKitInventory = true;
+                _emptyMedKitInventory = false;
                 break;
             default:
-                _full = false;
-                _empty = false;
+                _fullMedKitInventory = false;
+                _emptyMedKitInventory = false;
                 break;
         }
     }
-    
+
     private void UpdateMedKitBar()
     {
         _medKitBar.SetMedKitCount(_numberOfMedKits);
